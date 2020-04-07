bbf19d6d0b677969f319db3722fc545773dc7c07
Az3r <tuan99kghd@gmail.com>
2020-04-07 20:43:17 +0700

    commit
    1.java -> indexof(int)
    2.java -> indexof(int,int)

---
+++ 1.java
@@ -0,0 +1,6 @@
+public class testStr {
+        public static void main(String []args){
+            String str = "Wibu never die";
+            System.out.printf("Index of 'e' in string str is: %d",str.indexOf("e"));
+        }
+}
